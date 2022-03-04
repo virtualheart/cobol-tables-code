4e089a144ba86826fa6ef02cdac89579b0fcb5f0
Dhanajayan <72462261+virtualheart@users.noreply.github.com>
2022-03-04 12:26:30 +0530

    intial

---
+++ Tables.cob
@@ -0,0 +1,20 @@
+       IDENTIFICATION DIVISION.                                       
+       PROGRAM-ID. TABLES.                                            
+       DATA DIVISION.                                                 
+       FILE SECTION.                                                  
+       WORKING-STORAGE SECTION.                                       
+       01  WS-I       PIC 9(2).                                       
+       01  WS-J       PIC 9(2).                                       
+       01  WS-K       PIC ZZ9.                                        
+       PROCEDURE DIVISION.                                            
+       0001-MAIN-PROC.                                                
+           DISPLAY "TABLES CODE IN COBLE"                             
+           PERFORM VARYING WS-I FROM 1 BY 1 UNTIL WS-I > 10           
+           DISPLAY WS-I " TABLE'S"                                    
+           DISPLAY "*************"                                    
+              PERFORM VARYING WS-J FROM 1 BY 1 UNTIL WS-J > 10        
+               COMPUTE WS-K = WS-I * WS-J                             
+               DISPLAY WS-J " * " WS-I " =" WS-K                      
+              END-PERFORM                                             
+          END-PERFORM.      
+          STOP RUN.         
